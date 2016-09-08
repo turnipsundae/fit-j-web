a51e0c2b3e387eacea12450defb5e770d41e60aa
Kevin Lau <laudevelopment@gmail.com>
2016-09-07 22:17:51 -0700

    Initialize models, views and templates

--- workouts/views.py
+++ workouts/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Routine
 
 # Create your views here.
 def index(request):
-  return HttpResponse("This is the index page for workouts.")
+  latest_routine_list = Routine.objects.order_by('-pub_date')[:5]
+  context = {'latest_routine_list': latest_routine_list}
+  return render(request, 'workouts/index.html', context)
 
 def results(request, routine_id):
   response = "You're looking at the results of routine %s."
@@ -13,4 +17,5 @@ def like(request, routine_id):
   return HttpResponse("You're adding a +1 to routine %s." % routine_id)
 
 def detail(request, routine_id):
-  return HttpResponse("You're looking at the details for routine %s." % routine_id)
+  routine = get_object_or_404(Routine, pk=routine_id)
+  return render(request, 'workouts/detail.html', {'routine': routine})
