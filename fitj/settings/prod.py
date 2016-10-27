18ca85ec0334032b13171932f0c5d79e6140aedb
Kevin Lau <laudevelopment@gmail.com>
2016-10-26 20:16:45 -0700

    another attempt

--- fitj/settings/prod.py
+++ fitj/settings/prod.py
@@ -9,7 +9,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-from base import *
+from fitj.settings.base import *
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
