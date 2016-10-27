006c4e3cbbfd1548b964d53c53afda93e617d2a1
Kevin Lau <laudevelopment@gmail.com>
2016-10-26 18:52:56 -0700

    Restructure settings dir

---
+++ fitj/settings/prod.py
@@ -0,0 +1,19 @@
+"""
+Django settings for fitj project on Heroku. Fore more info, see:
+https://github.com/heroku/heroku-django-template
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.9/ref/settings/
+"""
+
+from base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Allow all host headers
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['fitj.herokuapp.com']
