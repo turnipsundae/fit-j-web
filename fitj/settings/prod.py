e5884bea0741039da52516ab9dbe5d2912591e14
Kevin Lau <laudevelopment@gmail.com>
2016-10-26 19:30:22 -0700

    Fix prod settings

--- fitj/settings/prod.py
+++ fitj/settings/prod.py
@@ -9,7 +9,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-from . import base
+from base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
