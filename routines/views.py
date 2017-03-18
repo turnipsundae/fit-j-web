7a715b2bb9fc53204e02f0ffe9d62e422ce6b1f9
Kevin Lau <laudevelopment@gmail.com>
2017-03-17 18:44:11 -0700

    ff

--- routines/views.py
+++ routines/views.py
@@ -6,7 +6,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return render(request, 'routines/index.html')
     # return render(request, 'index.html')
 
 
