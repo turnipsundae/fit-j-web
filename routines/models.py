21e820ddb450e0389b5aa13ccc430f9156c91172
Kevin Lau <laudevelopment@gmail.com>
2017-04-01 15:54:11 -0700

    Test add WODs and statistics to routines

--- routines/models.py
+++ routines/models.py
@@ -4,35 +4,35 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
-class Workout(Base):
+class Workout(models.Model):
   """Workout represents one WOD. May contain duplicates."""
   __tablename__ = 'workouts'
 
-  title = models.CharField()
+  title = models.CharField(max_length=255)
   description = models.TextField()
-  results_best = models.CharField(null=True)
-  results_worst = models.CharField(null=True)
-  results_avg = models.CharField(null=True)
-  results_best_male = models.CharField(null=True)
-  results_worst_male = models.CharField(null=True)
-  results_avg_male = models.CharField(null=True)
-  results_best_female = models.CharField(null=True)
-  results_worst_female = models.CharField(null=True)
-  results_avg_female = models.CharField(null=True)
+  results_best = models.CharField(max_length=255, null=True)
+  results_worst = models.CharField(max_length=255, null=True)
+  results_avg = models.CharField(max_length=255, null=True)
+  results_best_male = models.CharField(max_length=255, null=True)
+  results_worst_male = models.CharField(max_length=255, null=True)
+  results_avg_male = models.CharField(max_length=255, null=True)
+  results_best_female = models.CharField(max_length=255, null=True)
+  results_worst_female = models.CharField(max_length=255, null=True)
+  results_avg_female = models.CharField(max_length=255, null=True)
 
   def __repr__(self):
     return "<Workout(title='%s', description='%s')>" % (self.title, self.description)
 
-class User(Base):
+class User(models.Model):
   """Users that leave comments for the workout."""
   __tablename__ = 'users'
 
-  username = models.CharField() 
+  username = models.CharField(max_length=255) 
 
   def __repr__(self):
     return "<User(username='%s')>" % (self.username)
 
-class Comment(Base):
+class Comment(models.Model):
   """Comments for each workout."""
   __tablename__ = 'comments'
 
@@ -44,21 +44,21 @@ class Comment(Base):
     return "<Comment(text='%s')>" % (self.text)
 
 
-class Result(Base):
+class Result(models.Model):
   """Result of a user performing a workout"""
   __tablename__ = 'results'
 
   workout = models.ForeignKey(Workout, null=True)
   comment = models.ForeignKey(Comment, null=True)
   user = models.ForeignKey(User, null=True)
-  gender = models.CharField(null=True)
+  gender = models.CharField(max_length=255, null=True)
   age = models.IntegerField(null=True)
-  height = models.CharField(null=True)
+  height = models.CharField(max_length=255, null=True)
   weight = models.IntegerField(null=True)
-  result = models.CharField(null=True)
+  result = models.CharField(max_length=255, null=True)
   score = models.IntegerField(null=True)
-  units = models.CharField(null=True)
-  mods = models.CharField(null=True)
+  units = models.CharField(max_length=255, null=True)
+  mods = models.CharField(max_length=255, null=True)
 
   def __repr__(self):
     return "<Result('%s %s')>" % (self.result, self.units)
--- routines/views.py
+++ routines/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.db.models import F
+from django.urls import reverse
 
-from .models import Greeting
+from .models import Workout, User, Comment, Result
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'routines/index.html')
+    # params = {'workout_title': 'Fran',
+    #           'workout_description': '21-15-9 Pull Ups Thrusters 95#', 
+    #           'workout_results_best_male': '4:30', 
+    #           'workout_results_avg_male': '5:30',
+    #           'workout_results_worst_male': '7:30',
+    #           'workout_results_best_female': '5:55',
+    #           'workout_results_avg_female': '7:30',
+    #           'workout_results_worst_female': '10:00'}
+    workout = get_object_or_404(Workout, id=1)
+    params = {'workout': workout}
+    return render(request, 'routines/index_dynamic.html', params)
     # return render(request, 'index.html')
 
 
